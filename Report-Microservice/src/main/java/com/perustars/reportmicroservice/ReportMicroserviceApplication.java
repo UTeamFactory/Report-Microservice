8d169dc4bbc88740668b072f23bba5cc6ea96ef8
Adrian Esqueiros <u291718169@upc.edu.pe>
2022-06-17 20:45:58 -0500

    update properties

--- Report-Microservice/src/main/java/com/perustars/reportmicroservice/ReportMicroserviceApplication.java
+++ Report-Microservice/src/main/java/com/perustars/reportmicroservice/ReportMicroserviceApplication.java
@@ -4,16 +4,24 @@ import com.perustars.reportmicroservice.config.AxonConfig;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @OpenAPIDefinition
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableEurekaClient
 @Import({ AxonConfig.class })
 public class ReportMicroserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReportMicroserviceApplication.class, args);
     }
-
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder loadBalancedWebClientBuilder() {
+        return WebClient.builder();
+    }
 }
